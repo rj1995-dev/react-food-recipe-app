6553ed76b3334b6e573fc40d7b7d1a8489e9426a
rj1995-dev <ram95.jana@gmail.com>
2019-08-16 00:11:25 +0530

    Recipe page Setup

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
-
+import RecipeList from "../components/RecipeList";
+import Search from "../components/Search";
+import { recipeData } from "../data/tempList";
 export default class Recipes extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    recipes: recipeData,
+    search: ""
+  };
+  handleChange = e => {
+    this.setState({
+      search: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+  };
   render() {
-    return <h4>Hello From Recipes</h4>;
+    return (
+      <>
+        <Search
+          search={this.state.search}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <RecipeList recipes={this.state.recipes} />
+      </>
+    );
   }
 }
