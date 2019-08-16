05598bdccd51d1882c1fd476532fa6bc1450ff26
rj1995-dev <ram95.jana@gmail.com>
2019-08-16 23:30:52 +0530

    search funtionality

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import { recipeData } from "../data/tempDetails";
 import { Link } from "react-router-dom";
 export default class SingleRecipe extends Component {
   constructor(props) {
